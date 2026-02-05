6b960d3b7fb99ff0fd4236c7883497588e8163bc
oneachina <ruiruixi@hotmail.com>
2026-02-05 13:53:33 +0800

    feat(ClickGuiModule): Add Debug Setting and test4 setting

--- src/main/java/com/eatgrapes/hina/module/impl/render/ClickGuiModule.java
+++ src/main/java/com/eatgrapes/hina/module/impl/render/ClickGuiModule.java
@@ -11,13 +11,19 @@ import com.eatgrapes.hina.setting.*;
 
 public class ClickGuiModule extends Module {
     public static ColorSetting themeColor = new ColorSetting("Theme Color", 0xFF4CAF50);
+    public static ModeSetting test4 = new ModeSetting("Test 4", "a", "a", "b", "c");
+    public static BooleanSetting debug = new BooleanSetting("debug", false);
 
     public ClickGuiModule() {
         super("ClickGUI", Category.RENDER);
         addSetting(new BooleanSetting("Test 1", true));
         addSetting(new NumberSetting("Test 2", 1.0, 0.1, 5.0, 0.1));
         addSetting(new ModeSetting("Test 3", "Modern", "Modern", "Classic", "Minimal"));
+        addSetting(test4);
         addSetting(themeColor);
+        addSetting(debug);
+
+        test4.setMulti(true);
     }
     
     public static int getThemeColor() {
